8d31d91609797e4d5cff17e2083a10d5686f2da0
Samanyu13 <samanyusaji18@gmail.com>
2020-02-18 23:50:53 +0530

    Updating model

---
+++ routes/public/db_entry.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const methods = require('./../../methods');
+
+//private/db_entry/
+
+module.exports = router;
---
+++ routes/public/employee.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const methods = require('./../../methods');
+
+//private/employee/
+
+module.exports = router;
