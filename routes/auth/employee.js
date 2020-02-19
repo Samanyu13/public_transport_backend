61229d8031177cae4f1b7a01476bd09fd16d39c1
joeljosephreji <joeljosephreji@gmail.com>
2020-02-19 16:58:26 +0530

    adding employee side of things

---
+++ routes/auth/employee.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const methods = require('./../../methods');
+
+
+module.exports = router;
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -4,4 +4,7 @@ const router = express.Router();
 //auth/user
 router.use('/user', require('./user'));
 
+//auth/employee
+router.use('/employee', require('./employee'));
+
 module.exports = router;
