f81eff7489f25fda5c160dc01dbed124713b660d
Glitch (samanyu13-public-transport-backend) <none>
2020-04-28 09:46:33 +0000

    ☄️💏 Checkpoint ./models/index.js:3135162/10

--- models/index.js
+++ models/index.js
@@ -8,8 +8,8 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  // sequelize = new Sequelize(process.env[config.use_env_variable], config);
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  // sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(
     config.database,
